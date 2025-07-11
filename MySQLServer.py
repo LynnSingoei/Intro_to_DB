f876f87ca8105ed1fa96a125afc7c9c8095fdcce
Lynn Singoei <lynnsingoei@gmail.com>
2025-07-11 16:12:24 +0300

    "Handling MySQL errors"

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from mysql.connector import Error
 mydb=mysql.connector.connect(
     host="localhost",
     user="root",
@@ -10,8 +11,8 @@ try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
     mydb.commit()
     print("Database 'alx_book_store' created successfully!")
-except OperationalError as e:
-     print("Error details:", e)
+except Error as e:
+    print(f"Error creating database: {e}")
 
 finally:
      mycursor.close()
