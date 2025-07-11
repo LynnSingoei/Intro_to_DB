5fd3f29510317c72bbb68ade5887c012b1c84263
Lynn Singoei <lynnsingoei@gmail.com>
2025-07-11 14:51:17 +0300

    "Set up connection of sql with python"

---
+++ MySQLServer.py
@@ -0,0 +1,19 @@
+import mysql.connector
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="@Lynn4820"
+
+)
+mycursor=mydb.cursor()
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+    mydb.commit()
+    print("Database 'alx_book_store' created successfully!")
+except OperationalError as e:
+     print("Error details:", e)
+
+finally:
+     mycursor.close()
+     mydb.close()
+
